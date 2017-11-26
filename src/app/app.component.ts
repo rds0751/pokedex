aa662ad0923675713a004c2229783ac9d9048ae8
Ripudaman Singh <rds0751@gmail.com>
2017-11-26 11:15:09 +0530

    msg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
 import { Component } from '@angular/core';
 
+/**
+ * Import the Pokedex service.
+ */
+import { PokedexService } from './pokedex.service';
+
+/**
+ * Import the Pokemon class
+ */
+import { Pokemon } from './pokemon';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  /**
+   * The component maintains
+   * a list of Pokemon objects
+   * that will be rendered.
+   *
+   * We initialize it to an empty
+   * list.
+   */
+  pokemon: Pokemon[] = [];
+
+  /**
+   * A boolean that represents
+   * if we are currently loading data.
+   */
+  isLoading: boolean = false;
+
+ /**
+   * This boolean will be set
+   * to true if an error occurred.
+   */
+  error: boolean = false;
+
+  /**
+   * Inject the Pokedex service.
+   */
+  constructor(private pokedexService: PokedexService) { }
+
+  /**
+   * A lifecycle method
+   * that is automatically
+   * envoked when the component
+   * is created.
+   */
+  ngOnInit() {
+    /**
+     * Load the initial data.
+     */
+    this.loadMore();
+  }
+
+  loadMore() {
+    this.isLoading = true;
+
+    /**
+     * Use the Pokedex service
+     * to load the next 9 Pokemon.
+     */
+    this.pokedexService.getPokemon(this.pokemon.length, 9)
+      .then(pokemon => {
+        pokemon = pokemon.map(p => {
+          p.imageLoaded = false;
+          return p;
+        });
+                /**
+                  * If loading was successful
+         * we append the result to the list.
+         */
+        this.pokemon = this.pokemon.concat(pokemon);
+        this.isLoading = false;
+        this.error = false;
+      })
+      .catch(() => {
+        this.error = true;
+        this.isLoading = false;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ApplicationRef } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+/**
+ * The HTTP service is not added by default
+ * so we need to import it.
+ */
+import { HttpModule } from '@angular/http';
+
+/**
+ * Import the PokedexService
+ */
+import { PokedexService } from './pokedex.service';
+import { CapitalizePipe } from './capitalize.pipe';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizePipe
   ],
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    /**
+     * Register the HTTP service.
+     */
+    HttpModule
   ],
-  providers: [],
+  /**
+   * Register the service as a
+   * provider.
+   */
+  providers: [PokedexService],
   entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
